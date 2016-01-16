b4c00e5f30b45aff94307cf78ea147f2bb8a9f81
Marco Melletti <mellotanica@hotmail.it>
2016-01-17 00:07:04 +0100

    should be working

--- addon.py
+++ addon.py
@@ -1,5 +1,9 @@
 import xbmcgui
-import xbmc
-import sys
+import subprocess
+
+answer = xbmcgui.Dialog().yesno("Start RetroPie?", "Restart the system and boot into RetroPie menu?");
+if answer:
+    subprocess.call("/home/osmc/nextBootRetroPie")
+    xbmc.executebuiltin('Reboot()')
 
 
